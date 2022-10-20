3edb8360163dbfd0c98c85d4902550bb9ef74648
Ketki Keni <sketki220@gmail.com>
2022-10-20 15:16:54 +0530

    used loop to add numbers from 1 to 100

--- src/main/java/AddNumber.java
+++ src/main/java/AddNumber.java
@@ -1,5 +1,9 @@
 public class AddNumber {
     public static void main(String[] args) {
-
+        int sum = 0;
+        for(int num =1; num <= 100 ; num++){
+            sum = sum +num;
+        }
+        System.out.println("Sum of the numbers from 1 to 100 : " + sum);
     }
 }
